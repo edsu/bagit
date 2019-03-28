ac04be7f81658653c33035772315d5af1f2a8624
Ed Summers <ehs@pobox.com>
2019-03-28 17:02:14 -0400

    test listdir with subdirectory

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -15,3 +15,14 @@ def test_listdir():
     storage = FileSystemStorage(TEMP_DATA)
     dirs, files = storage.listdir()
     assert dirs == ['si', 'loc']
+    assert files == ['README']
+
+def test_listdir_subdir():
+    storage = FileSystemStorage(TEMP_DATA)
+    dirs, files = storage.listdir("loc")
+    assert dirs == []
+    assert files == [
+        '2478433644_2839c5e8b8_o_d.jpg',
+        '3314493806_6f1db86d66_o_d.jpg'
+    ]
+
