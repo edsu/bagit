fcee4ddecd0a24d09e890bc29ffe7df21803f4c2
Ed Summers <ehs@pobox.com>
2019-08-22 12:54:54 -0400

    https://www.youtube.com/watch?v=pZCxyOcvp5A

--- bagit/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cli import main
-from .bag import Bag
-from .manifest import Manifest
--- setup.py
+++ setup.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python
-# encoding: utf-8
 
 from setuptools import setup
 
 setup(
     name="bagit",
     version="2.0.0",
-    url="https://libraryofcongress.github.io/bagit-python/",
+    url="https://github.com/edsu/bagit/",
     author="Ed Summers",
     author_email="ehs@pobox.com",
     packages=["bagit"],
     description="Create and validate BagIt packages",
     long_description="bagit is a Python library and command line utility for working with BagIt style packages.",
     entry_points={"console_scripts": ["bagit = bagit.cli:main"]},
+    install_requires=['fs'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    classifiers=[
-        "License :: Public Domain",
-        "Intended Audience :: Developers",
-        "Topic :: Communications :: File Sharing",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Filesystems",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-    ],
 )
